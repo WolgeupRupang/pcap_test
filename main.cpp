e8214b8536f64137fc4a82b060b6ef36f58ba73f
WolgeupRupang <borngunk7061@naver.com>
2020-03-12 17:15:25 +0900

    add comment

--- main.cpp
+++ main.cpp
@@ -1,33 +1,47 @@
 #include <pcap.h>
 #include <stdio.h>
+#include <stdint.h>
 
 void usage() {
   printf("syntax: pcap_test <interface>\n");
   printf("sample: pcap_test wlan0\n");
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char* argv[]) { // starting command: /.pcap_test eth0 //
   if (argc != 2) {
     usage();
     return -1;
   }
 
-  char* dev = argv[1];
+  char* dev = argv[1]; //2nd input variable = address (eth0) //
   char errbuf[PCAP_ERRBUF_SIZE];
   pcap_t* handle = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf);
   if (handle == NULL) {
     fprintf(stderr, "couldn't open device %s: %s\n", dev, errbuf);
     return -1;
-  }
+  } //return handle//
 
   while (true) {
     struct pcap_pkthdr* header;
     const u_char* packet;
-    int res = pcap_next_ex(handle, &header, &packet);
+    int res = pcap_next_ex(handle, &header, &packet); //function that capture the next packet-re code it!//
     if (res == 0) continue;
     if (res == -1 || res == -2) break;
     printf("%u bytes captured\n", header->caplen);
   }
+
+/*
+output ex:
+
+printf("-------------Packet Capture-------------")
+printf("d_mac = "); //struct ethhdr//
+printf("s_mac = ");
+printf("s_ip = "); //struct iphdr//
+printf("d_ip = ");
+printf("s_port = "); //struct tcphdr//
+printf("d_port = ");
+
+*/
 
   pcap_close(handle);
   return 0;
